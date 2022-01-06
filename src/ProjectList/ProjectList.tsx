b9283f861581f796c4b3e924118f2587c773e13e
Nigel Alford <nigel.alford@wahoofitness.com>
2022-01-05 22:55:01 -0500

    getting nifty with flexbox

--- src/ProjectList/ProjectList.tsx
+++ src/ProjectList/ProjectList.tsx
@@ -3,7 +3,7 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import { Link, Typography } from '@mui/material';
+import { Button, Link, Typography } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -13,7 +13,7 @@ const Item = styled(Paper)(({ theme }) => ({
   height: '4em',
 }));
 const description =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
 
 const logo =
   'https://uploads-ssl.webflow.com/60d83e3c6cf84748f7d0a62b/61aa33f03400541381c2b2f5_MyHomePlanet.png';
@@ -41,21 +41,23 @@ interface Props {
 export default function ProjectList(props: Props) {
   return (
     <Box sx={{ flexGrow: 1 }} style={{ marginTop: '2em' }}>
-      <Typography style={{ textAlign: 'left' }} variant="h5" gutterBottom component="div">
-        {props.title}
-      </Typography>
-      {props.CTA && (
-        <Typography style={{ textAlign: 'right' }} variant="h5" gutterBottom component="div">
-          <Link href="#" variant="body2">
-            {props.CTA}
-          </Link>
+      <div style={{ display: 'flex' }}>
+        <Typography variant="h5" gutterBottom component="div">
+          {props.title}
         </Typography>
-      )}
+        {props.CTA && (
+          <Typography sx={{ marginLeft: 'auto' }} variant="h5" gutterBottom component="div">
+            <Link href="#" variant="body2">
+              {props.CTA}
+            </Link>
+          </Typography>
+        )}
+      </div>
 
       <Grid container spacing={2}>
         {testData.map((td, i) => (
           <Grid item xs={12} sm={3} key={td.title + i}>
-            <Item sx={{ height: '14em', textAlign: 'initial' }}>
+            <Item sx={{ height: '14em', display: 'flex', flexWrap: 'wrap' }}>
               <div style={{ display: 'flex', alignItems: 'center' }}>
                 <img src={td.logo} alt="coolLogo" style={{ width: '3em' }} />
                 <Typography
@@ -76,6 +78,9 @@ export default function ProjectList(props: Props) {
               >
                 {td.description}
               </Typography>
+              <Button variant="contained" sx={{ marginTop: '1em', marginLeft: 'auto' }}>
+                Learn more
+              </Button>
             </Item>
           </Grid>
         ))}
